c257da7d4f1930a23f61f0be7bd8f6b0a9ad2cd1
Marlee McInelly <marleemcinelly@gmail.com>
2021-02-01 09:13:14 -0700

    boilerplate server code added to server.js

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var fs = require("fs");
+
+var PORT = process.env.PORT || 3001;
+
+var server = http.createSevrer(handleRequest);
+
+function handlerequest(req, res) {
+    fs.readFile(__dirname + "/index.html", function (err, data) {
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+    });
+}
+
+server.listen (PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+})
