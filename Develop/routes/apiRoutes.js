5644fadc307b2a1d2397dcc7b9cfc90b777f5863
Marlee McInelly <marleemcinelly@gmail.com>
2021-02-02 18:09:39 -0700

    api and html routes files written

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,23 @@
+// LOAD DATA
+var database = require("../db/db");
+var fs = require("fs");
+
+// ROUTING
+module.exports = function(app){
+
+    // GET function
+    app.get("/api/notes", function(req, res) {
+        res.json(database);
+    });
+
+    // POST function
+    app.post("/api/notes", function(req, res){
+        database.push(req.body);
+        res.json(true);
+    });
+
+    // DELETE function
+    app.delete("/api/notes/:id", function(req, res){
+        // gotta figure out how to read unique ids, which will need to be assigned in the above functions because god is laughing at my pain
+    })
+};
--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,18 @@
+// DEPENDENCIES
 var express = require("express");
 var app = express();
-var fs = require("fs");
 
+// EXPRESS CONFIGURATION
 var PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
-require("./db/db")(app);
+// ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
+// LISTENER
 app.listen (PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
 })
