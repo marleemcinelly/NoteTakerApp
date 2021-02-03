6c9be35896ac537956e2169c9084866b3de018cf
Marlee McInelly <marleemcinelly@gmail.com>
2021-02-02 17:35:47 -0700

    linked db file

--- Develop/server.js
+++ Develop/server.js
@@ -7,6 +7,8 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
+require("./db/db")(app);
+
 app.listen (PORT, function() {
     console.log("Server is listening on PORT: " + PORT);
 })
